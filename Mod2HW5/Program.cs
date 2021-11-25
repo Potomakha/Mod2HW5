51622d6ab25f3fcbc8b7b023b751c8f617bc0a7a
Oleh Potomakha <potomakha@gmail.com>
2021-11-25 12:35:58 +0200

    Add project files.

---
+++ Mod2HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod2HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
